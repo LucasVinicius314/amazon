d0ec25f7174ca1a2e82f3187f5b7091f62ae28ba
Vinícius Henrique Giovanini <vhgiovanini@gmail.com>
2023-05-25 11:37:55 -0300

    branch and bound iniciado (quebradao)

--- App.java
+++ App.java
@@ -31,38 +31,39 @@ public class App {
 
     // Calcular todos os caminhos possíveis.
     // Já pega somente os caminhos validos
-    final var allPaths = Solver
-        .getPermutations(false)
-        .stream()
-        .map(e -> new Path(e, maxCargo))
-        .sorted((a, b) -> (int) Math.round(a.cost - b.cost))
-        .collect(Collectors.toList());
+    // final var allPaths = Solver
+    // .getPermutations(false)
+    // .stream()
+    // .map(e -> new Path(e, maxCargo))
+    // .sorted((a, b) -> (int) Math.round(a.cost - b.cost))
+    // .collect(Collectors.toList());
 
     // Filtrar caminhos possíveis, que permitem todas as entregas sem estourar o
     // limite de carga do caminhão e sem passar em um node mais de uma vez.
-    final var possiblePaths = Solver
-        .getPermutations(true)
-        .stream()
-        .map(e -> new Path(e, maxCargo))
-        .sorted((a, b) -> (int) Math.round(a.cost - b.cost))
-        .collect(Collectors.toList());
+    // final var possiblePaths = Solver
+    // .getPermutations(true)
+    // .stream()
+    // .map(e -> new Path(e, maxCargo))
+    // .sorted((a, b) -> (int) Math.round(a.cost - b.cost))
+    // .collect(Collectors.toList());
 
-    // Printar o caminho da melhor permutação
-    System.out.print("Melhor caminho: ");
-    for (Node node : possiblePaths.get(0).nodes) {
-      System.out.print("[" + node.key + "], ");
+    // // Printar o caminho da melhor permutação
+    // System.out.print("Melhor caminho: ");
+    // for (Node node : possiblePaths.get(0).nodes) {
+    // System.out.print("[" + node.key + "], ");
 
-    }
-    System.out.print("Custo: " + possiblePaths.get(0).cost);
-    "".toString();
+    // }
+    // System.out.print("Custo: " + possiblePaths.get(0).cost);
+    // "".toString();
 
     // Iniciando branch and bound
 
-    // final var allPathsBranchBound = Bound
-    // .getPermutations()
-    // .stream()
-    // .map(e -> new Path(e, maxCargo))
-    // .collect(Collectors.toList());
+    final var allPathsBranchBound = Bound
+        .getPermutations(maxCargo)
+        .stream()
+        .collect(Collectors.toList());
+
+    "".toString();
 
   }
 
--- Bound.java
+++ Bound.java
@@ -6,7 +6,7 @@ import java.util.List;
 // Mofificar a classe para realizar a permutacao testando qual é o menor elemento
 public class Bound {
 
-  public static List<List<Node>> getPermutations() {
+  public static List<List<Node>> getPermutations(int maxCargo) {
     final var nodes = new ArrayList<>(App.nodes.values());
 
     final var rootNode = App.nodes.get(0);
@@ -15,9 +15,25 @@ public class Bound {
 
     final var permutatedPaths = permutate(nodes);
 
-    for (final var permutatedPath : permutatedPaths) {
+    double minCost = 100000;
+
+    // Até o momento ele está pegando o caminho com menor custo
+    // TA VOLTANDO VARIOS (ERRO)
+    // Analisar como é implementado o custo no node.
+    for (final var permutatedPath : new ArrayList<>(permutatedPaths)) {
       permutatedPath.add(0, rootNode);
       permutatedPath.add(rootNode);
+
+      Path amigo_path = new Path(permutatedPath, maxCargo);
+
+      if (amigo_path.cost < minCost) {
+        minCost = amigo_path.cost;
+
+      } else {
+        permutatedPaths.remove(permutatedPath);
+      }
+
+      "".toString();
     }
 
     return permutatedPaths;
